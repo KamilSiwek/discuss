3cc9d66ca66c087519e7401b7d901472ae445afe
KamilSiwek <kontokamilsiwek@gmail.com>
2018-09-17 17:04:12 +0200

    Add edit_page and edit helper to controller

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -7,6 +7,13 @@ defmodule Discuss.TopicController do
   # "Topic.changeset(%Topic{}, %{})""
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    # Disciss.Repo.all(Discuss.Topic)
+    topics = Repo.all(Topic)
+
+    render conn, "index.html", topics: topics
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
@@ -20,9 +27,35 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
       {:error, changeset} ->
         render conn, "new.html", changeset: changeset
+    end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    # old_topic = Repo.get(Topic, topic_id)
+    # changeset = Topic.changeset(old_topic, topic)
+    # or alternative:
+    changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset
     end
   end
 end
