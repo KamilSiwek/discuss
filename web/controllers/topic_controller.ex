e161687a491ebdb427f3847d9635050bc91d7704
KamilSiwek <kontokamilsiwek@gmail.com>
2018-09-17 14:48:49 +0200

    Add funcionality to add new topic to db - create new_view

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,10 +1,28 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
-  # When use alias you can change for example %Discuss.Topic{} for %Topic{}
+  # When use alias you can change for example:
+  # "Elixir.Topic.changeset(%Elixir.Topic{}, %{})"
+  # for
+  # "Topic.changeset(%Topic{}, %{})""
   alias Discuss.Topic
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  # Alternative for:
+  # def create(conn, params) do
+     # %{"topic" => topic} = params
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
